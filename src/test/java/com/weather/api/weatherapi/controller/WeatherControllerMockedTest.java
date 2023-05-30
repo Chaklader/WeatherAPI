3e4d3e832257927ca73b923321f7d31ff5c44029
“Chaklader <CAREFE-EXT@DRIVEWEALTH.TECH>
2023-05-30 17:36:29 +0600

    Fix

--- src/test/java/com/weather/api/weatherapi/controller/WeatherControllerMockedTest.java
+++ src/test/java/com/weather/api/weatherapi/controller/WeatherControllerMockedTest.java
@@ -2,6 +2,7 @@ package com.weather.api.weatherapi.controller;
 
 import com.weather.api.weatherapi.controller.dto.Coordinate;
 import com.weather.api.weatherapi.controller.dto.SimplifiedWeatherData;
+import com.weather.api.weatherapi.controller.dto.WeatherDataDto;
 import com.weather.api.weatherapi.service.LocationService;
 import com.weather.api.weatherapi.service.WeatherService;
 import org.apache.commons.lang3.tuple.Pair;
@@ -15,9 +16,10 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.mockito.ArgumentMatchers.any;
+import java.util.Date;
+import java.util.List;
 
-import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.ArgumentMatchers.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -28,7 +30,7 @@ import static org.hamcrest.Matchers.equalTo;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @AutoConfigureMockMvc
-class WeatherControllerTest {
+class WeatherControllerMockedTest {
 
     @Autowired
     private MockMvc mockMvc;
@@ -59,6 +61,44 @@ class WeatherControllerTest {
             .andExpect(jsonPath("$.feelsLike", equalTo(33.72)))
             .andExpect(jsonPath("$.windSpeed", equalTo(3.09)));
 
+    }
+
+
+    @Test
+    public void test_getHistoricalWeatherByCoordinates() throws Exception {
+
+        WeatherDataDto weatherDataDto = WeatherDataDto.builder()
+            .currentTemperature(304.18)
+            .minTemperature(304.18)
+            .maxTemperature(304.18)
+            .feelsLike(306.02)
+            .humidity(51)
+            .pressure(1006)
+            .visibility(4000)
+            .windSpeed(3.09)
+            .queryTimestamp(new Date())
+            .ipAddress("103.150.26.242")
+            .build();
+
+        List<WeatherDataDto> weatherDataDtosList = List.of(weatherDataDto);
+
+
+        Mockito.when(weatherService.getHistoricalWeatherByCoordinates(anyDouble(), anyDouble())).thenReturn(weatherDataDtosList);
+
+        mockMvc.perform(get("http://localhost:" + 8080 + "/v1/api/weather/coordinates?latitude=23.7&longitude=90.4")
+                .header("X-Forwarded-For", "103.150.26.242"))
+            .andExpect(status().isOk())
+            .andExpect(jsonPath("$").isArray())
+            .andExpect(jsonPath("$[0].currentTemperature").value(304.18))
+            .andExpect(jsonPath("$[0].minTemperature").value(304.18))
+            .andExpect(jsonPath("$[0].maxTemperature").value(304.18))
+            .andExpect(jsonPath("$[0].feelsLike").value(306.02))
+            .andExpect(jsonPath("$[0].humidity").value(51))
+            .andExpect(jsonPath("$[0].pressure").value(1006))
+            .andExpect(jsonPath("$[0].visibility").value(4000))
+            .andExpect(jsonPath("$[0].windSpeed").value(3.09))
+            .andExpect(jsonPath("$[0].queryTimestamp").exists())
+            .andExpect(jsonPath("$[0].ipAddress").value("103.150.26.242"));
     }
 
 
